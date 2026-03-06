6c1efd793a60c9081f4106c9ec936d565f33cbd5
caioLeone <caio_leone@hotmail.com>
2026-03-06 18:32:21 -0300

    feat: create Card Func

--- main.go
+++ main.go
@@ -15,6 +15,10 @@ type Deck struct {
 	Cards []Card //SLICE
 }
 
+var cards []Card
+var deck Deck
+var nextID int64 = 1
+
 func main() {
 
 }
@@ -22,11 +26,18 @@ func main() {
 func GameMenu() {
 	for {
 		fmt.Println("")
-		fmt.Println("")
-		fmt.Println("")
-		fmt.Println("")
-		fmt.Println("")
-		fmt.Println("")
+		fmt.Println("===== MENU =====")
+		fmt.Println("1 - Criar Carta")
+		fmt.Println("2 - Listar Carta")
+		fmt.Println("3 - Editar Carta")
+		fmt.Println("4 - Deletar Carta")
+		fmt.Println("===== DECK MENU =====")
+		fmt.Println("5 - Criar Deck")
+		fmt.Println("6 - Listar Deck")
+		fmt.Println("7 - Editar Deck")
+		fmt.Println("8 - Deletar Deck")
+		fmt.Println("9 - Salvar Deck em JSON")
+		fmt.Println("==========")
 		fmt.Println("0 - Sair")
 
 		var option int
@@ -34,6 +45,23 @@ func GameMenu() {
 
 		switch option {
 		case 1:
+			CreateCard()
+		case 2:
+			ListCard()
+		case 3:
+			EditCard()
+		case 4:
+			DeleteCard()
+		case 5:
+			CreateDeck()
+		case 6:
+			ListDeck()
+		case 7:
+			EditDeck()
+		case 8:
+			DeleteDeck()
+		case 9:
+			SaveJson()
 		case 0:
 			fmt.Println("Saindo...")
 			return
@@ -42,3 +70,40 @@ func GameMenu() {
 		}
 	}
 }
+
+func SaveJson() {}
+
+func CreateCard() {
+	var name string
+	var cardType string
+	var attack int64
+	var defence int64
+
+	fmt.Println("Nome da carta:")
+	fmt.Scan(&name)
+	fmt.Println("Tipo da carta:")
+	fmt.Scan(&cardType)
+	fmt.Println("Ataque da carta:")
+	fmt.Scan(&attack)
+	fmt.Println("Defesa da carta:")
+	fmt.Scan(&defence)
+
+	card := Card{
+		ID:      nextID,
+		Name:    name,
+		Type:    cardType,
+		Attack:  attack,
+		Defence: defence,
+	}
+
+	nextID++
+	cards = append(cards, card)
+	fmt.Println("Carta Forjada Com Sucesso")
+}
+func ListCard()   {}
+func EditCard()   {}
+func DeleteCard() {}
+func CreateDeck() {}
+func EditDeck()   {}
+func ListDeck()   {}
+func DeleteDeck() {}
